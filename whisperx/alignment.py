38509d2881322a57e8229d6c0168ff123a66aa13
Ubuntu <podwise@transcriber.up32bgmdfqzulfalyig4d4lpsa.phxx.internal.cloudapp.net>
2024-04-03 22:02:31 +0000

    Fixes alignment bug about returnning empty batch.

--- whisperx/alignment.py
+++ whisperx/alignment.py
@@ -516,7 +516,8 @@ def find_alignments(model, tokenizer, text_tokens, encoder_output, median_filter
         )
         word_boundaries = np.pad(np.cumsum([len(t) for t in word_tokens[:-1]]), (1, 0))
         if len(word_boundaries) <= 1:
-            return []
+            final_alignments.append([])
+            continue
 
         jumps = np.pad(np.diff(text_indices), (1, 0), constant_values=1).astype(bool)
         jump_times = time_indices[jumps] / TOKENS_PER_SECOND 
